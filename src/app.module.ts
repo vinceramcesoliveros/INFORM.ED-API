c232e355a7814926a87f4fd11f3e9dd2a6f9ef2c
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-12-10 17:13:01 +0800

    remove unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import config from './config/settings';
 import { RolesModule } from './roles/roles.module';
 import { CourseModule } from './course/courses.module';
 import { StudentModule } from './student/student.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 @Module({
   imports: [
     AccountsModule,
