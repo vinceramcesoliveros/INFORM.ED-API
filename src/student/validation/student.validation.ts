77d50b2e0bdbbe39f808b589660638e2b5c2971f
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-12-10 22:10:01 +0800

    basic validation(use-cases-like) class

---
+++ src/student/validation/student.validation.ts
@@ -0,0 +1,34 @@
+import { StudentDto } from '../dto/student.dto';
+import { Service } from 'src/typings/service.implementation';
+import { HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
+
+export class StudentValidation {
+  constructor(private readonly studentDto?: StudentDto) {}
+
+  serviceExists(serviceValidation: Readonly<ServiceValidation<any>[]>) {
+    const error = [];
+    for (const serviceSchema of serviceValidation) {
+      if (
+        !serviceSchema.service.findOne(this.studentDto[serviceSchema.schema])
+      ) {
+        error.push({
+          name: serviceSchema.schema,
+          message: `${serviceSchema.schema} not found!`,
+        });
+      }
+    }
+    if (error.length) {
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+  public validateId(id: string) {
+    if (!id) {
+      throw new BadRequestException('Id must be provided', id);
+    }
+  }
+}
+
+interface ServiceValidation<T> {
+  schema: string;
+  service: Service<T>;
+}
