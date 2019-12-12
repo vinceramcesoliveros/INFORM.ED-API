46e26eb0a7072322fe5912643b8a2591a59845f5
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-12-12 22:38:13 +0800

    model validation(FindById,Update & Delete)

---
+++ src/validation/model.validation.ts
@@ -0,0 +1,94 @@
+import { StudentDto } from '../student/dto/student.dto';
+import { Service } from 'src/typings/service.implementation';
+import { HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
+import { Student } from '../student/interface/student.interface';
+import { DataTransferObject } from 'src/typings/dto.implementation';
+/**
+ * Add a scenario when a particular id is not a valid ID or it does not
+ * exists from the database to ensure that there can be no errors.
+ *
+ */
+export class ModelValidation {
+  private readonly dataTransferObject?: DataTransferObject;
+  private readonly serviceValidation?: ServiceValidation<any>[];
+  constructor({
+    dataTransferObject,
+    serviceValidation,
+  }: ServiceValidator = {}) {
+    this.serviceValidation = serviceValidation;
+    this.dataTransferObject = dataTransferObject;
+  }
+
+  serviceExists() {
+    if (!this.serviceValidation.length) return;
+    const error = [];
+    for (const serviceSchema of this.serviceValidation) {
+      if (
+        !serviceSchema.service.findOne(
+          this.dataTransferObject[serviceSchema.schema],
+        )
+      ) {
+        error.push({
+          name: serviceSchema.schema,
+          message: `${serviceSchema.schema} not found!`,
+        });
+      }
+    }
+    if (error.length) {
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+  /**
+   * Query Type does the following for the chosen service,
+   * - `FINDBYID(ID)`
+   * - `UPDATE(ID,BODY)`
+   * - `DELETE(ID)`
+   *
+   */
+  public async validateId({ id, service, type, body }: ValidationQuery<any>) {
+    try {
+      if (!id) {
+        throw `Id must be provided`;
+      }
+      if (body) {
+        return await service[`${type}`](id, body);
+      }
+
+      return await service[`${type}`](id);
+    } catch (error) {
+      throw new BadRequestException(error, id);
+    }
+  }
+
+  /**
+   *
+   * Validate only the type of a column you want to compare.
+   */
+  public validate({ value, column }: ValidationColumn) {
+    if (!value) {
+      throw new BadRequestException();
+    }
+    if (value !== column) {
+      throw new BadRequestException(`The value must be a(n) "${column}"`);
+    }
+  }
+}
+
+interface ServiceValidation<T> {
+  schema: string;
+  service: Service<T>;
+}
+interface ServiceValidator {
+  dataTransferObject?: DataTransferObject;
+  serviceValidation?: ServiceValidation<any | null>[];
+}
+interface ValidationColumn {
+  value: string | number | boolean;
+  column: string | number | boolean;
+}
+interface ValidationQuery<T> {
+  service: Service<T>;
+  type: 'update' | 'delete' | 'findOne';
+  id: string;
+  body?: DataTransferObject;
+}
