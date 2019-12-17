12efdbcc7e771238d3f6cf27144a220ddcc2ca33
Vince Ramces V. Oliveros <blusea231@gmail.com>
2019-12-17 20:03:56 +0800

    add email prop interface

--- src/accounts/interface/accounts.interface.ts
+++ src/accounts/interface/accounts.interface.ts
@@ -1,6 +1,6 @@
 import { Gender } from './enums';
 import { Document } from 'mongoose';
-import { Role } from 'src/roles/interface/roles.interface';
+
 export interface Account extends Document {
   readonly username?: string;
   readonly password?: string;
@@ -9,4 +9,5 @@ export interface Account extends Document {
   readonly lastName?: string;
   readonly gender?: Gender;
   readonly role?: string;
+  readonly email?: string;
 }
